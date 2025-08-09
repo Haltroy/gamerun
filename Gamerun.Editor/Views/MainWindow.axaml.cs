610af83599c8d467b19816303fdbf50fb27cac6a
haltroy <thehaltroy@gmail.com>
2025-08-09 23:20:41 +0300

    GUI: Add borders & prettify

--- Gamerun.Editor/Views/MainWindow.axaml.cs
+++ Gamerun.Editor/Views/MainWindow.axaml.cs
@@ -38,4 +38,44 @@ public partial class MainWindow : Window
         if (sender is not Button { Tag: Control c } || !ContentCarousel.Items.Contains(c)) return;
         ContentCarousel.SelectedItem = c;
     }
+
+    private void TopLeftPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        BeginResizeDrag(WindowEdge.NorthWest,e);
+    }
+
+    private void TopRightPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        BeginResizeDrag(WindowEdge.NorthEast,e);
+    }
+
+    private void TopPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        BeginResizeDrag(WindowEdge.North,e);
+    }
+
+    private void BottomLeftPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        BeginResizeDrag(WindowEdge.SouthWest,e);
+    }
+
+    private void BottomRightPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        BeginResizeDrag(WindowEdge.SouthEast,e);
+    }
+
+    private void BottomPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        BeginResizeDrag(WindowEdge.South,e);
+    }
+
+    private void LeftPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        BeginResizeDrag(WindowEdge.West,e);
+    }
+
+    private void RightPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        BeginResizeDrag(WindowEdge.East,e);
+    }
 }
